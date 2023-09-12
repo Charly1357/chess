075bdb7a829b06ab3a4b0fbe95921712e788648e
Charly1357 <>
2023-09-12 12:22:53 +0000

    chess

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 const gameboard = document.querySelector(".gameboard")
 const playerdisplay = document.querySelector('.player')
-const infoDisplay= document.querySelector('.info-display');
+const infoDisplay = document.querySelector('.info-display')
+const width = 8
